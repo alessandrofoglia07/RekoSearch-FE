7ae5d731ddf3df7624151bde1356d6178d80b401
alessandrofoglia07 <alessandrofoglia07@gmail.com>
2024-12-14 22:54:32 +0100

    Add initial pages configuration

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import HomePage from '@/pages/MainPage';
+import RegisterPage from '@/pages/account/RegisterPage';
+import LoginPage from '@/pages/account/LoginPage';
 
 const App: React.FC = () => {
     return (
         <Routes>
-            <Route path='*' element={<HomePage />} />
+            <Route path='/' element={<HomePage />} />
+
+            <Route path='/account/register' element={<RegisterPage />} />
+            <Route path='/account/login' element={<LoginPage />} />
+
+            <Route path='*' element={<Navigate to='/' replace />} />
         </Routes>
     );
 };
