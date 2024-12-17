5ad57d277b128a4f926c60399ed52c2ea4117944
alessandrofoglia07 <alessandrofoglia07@gmail.com>
2024-12-17 23:05:51 +0100

    Add skeleton Register page

--- src/pages/account/RegisterPage.tsx
+++ src/pages/account/RegisterPage.tsx
@@ -1,7 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+interface FormData {
+    name: string;
+    email: string;
+    password: string;
+}
 
 const RegisterPage: React.FC = () => {
-    return <div>RegisterPage</div>;
+    const [formData, setFormData] = useState<FormData>({
+        name: '',
+        email: '',
+        password: ''
+    });
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setFormData((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+    };
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement> | React.MouseEvent<HTMLButtonElement>) => {
+        e.preventDefault();
+        console.log(formData);
+    };
+
+    return (
+        <div>
+            <h1>Register Page</h1>
+            <form onSubmit={handleSubmit} autoComplete='off' spellCheck={false}>
+                <input type='text' name='name' autoFocus placeholder='Name' value={formData.name} onChange={handleChange} />
+                <input type='email' name='email' placeholder='Email' value={formData.email} onChange={handleChange} />
+                <input type='password' name='password' value={formData.password} onChange={handleChange} />
+            </form>
+            <button onClick={handleSubmit}>Register</button>
+            <a href='/login'>
+                Already have an account? <span>Login</span>
+            </a>
+        </div>
+    );
 };
 
 export default RegisterPage;
