aa5cbe8e1cd6d7e73a2c5998fa354af093ed6f57
alessandrofoglia07 <alessandrofoglia07@gmail.com>
2024-12-19 23:32:02 +0100

    Add skeleton for LoginPage

--- src/pages/account/LoginPage.tsx
+++ src/pages/account/LoginPage.tsx
@@ -1,7 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+interface FormData {
+    email: string;
+    password: string;
+}
 
 const LoginPage: React.FC = () => {
-    return <div>LoginPage</div>;
+    const [formData, setFormData] = useState<FormData>({
+        email: '',
+        password: ''
+    });
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setFormData((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+    };
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement> | React.MouseEvent<HTMLButtonElement>) => {
+        e.preventDefault();
+        console.log(formData);
+    };
+
+    return (
+        <div>
+            <h1>Login Page</h1>
+            <form onSubmit={handleSubmit} autoComplete='off' spellCheck={false}>
+                <input type='email' name='email' placeholder='Email' value={formData.email} onChange={handleChange} />
+                <input type='password' name='password' value={formData.password} onChange={handleChange} />
+            </form>
+            <button onClick={handleSubmit}>Login</button>
+            <a href='/login'>
+                Don't have an account? <span>Register</span>
+            </a>
+        </div>
+    );
 };
 
 export default LoginPage;
