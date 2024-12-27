dd0ff2989e67f9694a4e242fa76ba3e7018e929e
alessandrofoglia07 <alessandrofoglia07@gmail.com>
2024-12-27 22:59:07 +0100

    Start implementing confirmCode function for email confirmation

--- src/pages/account/ConfirmCodePage.tsx
+++ src/pages/account/ConfirmCodePage.tsx
@@ -1,8 +1,35 @@
-import React from 'react';
-import { useSearchParams } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import useRedirectToAccount from '@/hooks/useRedirectToAccount';
+import { confirmCodeSchema } from '@/utils/schemas/authSchemas';
+import { useNavigate, useSearchParams } from 'react-router-dom';
+import { CognitoUser } from 'amazon-cognito-identity-js';
+import userPool from '@/utils/userPool';
 
 const ConfirmCodePage: React.FC = () => {
+    useRedirectToAccount();
+
+    const navigate = useNavigate();
     const [searchParams] = useSearchParams();
+
+    const confirmCode = async () => {
+        const code = searchParams.get('code');
+        if (!code) return; // TODO: handle error
+
+        const codeVal = confirmCodeSchema.safeParse(code);
+        if (!codeVal.success) return; // TODO: handle error
+
+        const name = searchParams.get('name');
+        if (!name) return; // TODO: handle error
+
+        const cognitoUser = new CognitoUser({ Username: name, Pool: userPool });
+
+        cognitoUser.confirmRegistration(code, true, (err?: Error) => {
+            if (err) {
+                // TODO: handle error
+            }
+            navigate('/account/login'); // TODO: implement automatic login
+        });
+    };
 
     return <div>ConfirmCodePage</div>;
 };
