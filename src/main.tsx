b340e29a16c8c5894325aea04b25c4ae0ed46023
alessandrofoglia07 <alessandrofoglia07@gmail.com>
2024-12-29 22:58:12 +0100

    Setup AuthProvider in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,22 @@ import ReactDOM from 'react-dom/client';
 import App from '@/components/App';
 import '@/style.css';
 import { BrowserRouter } from 'react-router-dom';
-import { Account } from './context/AccountContext';
+import { AuthProvider } from 'react-oidc-context';
+
+const cognitoAuthConfig = {
+    authority: `https://cognito-idp.${import.meta.env.VITE_AWS_REGION}.amazonaws.com/${import.meta.env.VITE_COGNITO_USER_POOL_ID}`,
+    client_id: import.meta.env.VITE_COGNITO_APP_CLIENT_ID,
+    redirect_uri: `${import.meta.env.VITE_APP_URL}/callback`, // TODO: implement callback page
+    response_type: 'code',
+    scope: 'email openid phone'
+};
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
         <BrowserRouter>
-            <Account>
+            <AuthProvider {...cognitoAuthConfig}>
                 <App />
-            </Account>
+            </AuthProvider>
         </BrowserRouter>
     </React.StrictMode>
 );
