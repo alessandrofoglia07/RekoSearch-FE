3a980fbf0b99f4af332316b162328172d68a13d4
alessandrofoglia07 <alessandrofoglia07@gmail.com>
2025-01-09 23:16:27 +0100

    Add PrivateRoutes component

---
+++ src/context/PrivateRoutes.tsx
@@ -0,0 +1,25 @@
+import useAuth from '@/hooks/useAuth';
+import React, { useEffect, useState } from 'react';
+import { Navigate, Outlet } from 'react-router-dom';
+
+const PrivateRoutes: React.FC = () => {
+    const [isAuth, setIsAuth] = useState<boolean | undefined>(undefined);
+    const { getSession } = useAuth();
+
+    useEffect(() => {
+        (async () => {
+            try {
+                const session = await getSession();
+                setIsAuth(!!session);
+            } catch (err) {
+                setIsAuth(false);
+            }
+        })();
+    }, [getSession]);
+
+    if (isAuth === undefined) return null;
+
+    return isAuth ? <Outlet /> : <Navigate to='/account/signin' replace />;
+};
+
+export default PrivateRoutes;
