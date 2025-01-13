cc4101b11c35318a25c6b0793bb1b47b47dfc7a0
alessandrofoglia07 <alessandrofoglia07@gmail.com>
2025-01-13 22:12:39 +0100

    Start navbar skeleton

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
 
 const Navbar: React.FC = () => {
-    return <div>Navbar</div>;
+    return (
+        <nav className='fixed left-0 top-0 z-50 m-4 mb-8 flex h-12 w-[calc(100svw-2rem)] items-center justify-between'>
+            <a className='h-full pr-4 md:pr-8' href='/'>
+                RekoSearch
+            </a>
+            <div className='flex h-full items-center md:pr-8 -md:w-3/4'></div>
+            <div className='flex h-full w-10 items-center justify-end md:w-[calc(3rem+150px)]'></div>
+        </nav>
+    );
 };
 
 export default Navbar;
