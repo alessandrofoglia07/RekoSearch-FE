548a530250ad6248091ee2d7c573d7513bac655b
alessandrofoglia07 <alessandrofoglia07@gmail.com>
2025-01-14 22:43:12 +0100

    Integrate Navbar in mainpage and accountpage

--- src/pages/AccountPage.tsx
+++ src/pages/AccountPage.tsx
@@ -1,7 +1,17 @@
+import Navbar from '@/components/Navbar';
 import React from 'react';
 
 const AccountPage: React.FC = () => {
-    return <div>AccountPage</div>;
+    return (
+        <div>
+            <header className='h-20'>
+                <Navbar />
+            </header>
+            <main>
+                <div className='mx-auto my-24 max-w-[30rem] rounded-md p-6 shadow-sm sm:p-12 md:max-w-[40rem] md:p-24 lg:max-w-[60rem]'>AccountPage</div>
+            </main>
+        </div>
+    );
 };
 
 export default AccountPage;
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -4,8 +4,12 @@ import Navbar from '@/components/Navbar';
 const MainPage: React.FC = () => {
     return (
         <div>
-            <Navbar />
-            <h1>MainPage</h1>
+            <header className='h-20'>
+                <Navbar />
+            </header>
+            <main>
+                <div className='mx-auto my-24 max-w-[30rem] rounded-md p-6 shadow-sm sm:p-12 md:max-w-[40rem] md:p-24 lg:max-w-[60rem]'>MainPage</div>
+            </main>
         </div>
     );
 };
