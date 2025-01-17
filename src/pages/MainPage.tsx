afc8bbfb438ac69efabb1721e13ef4c8cd8ed5b3
alessandrofoglia07 <alessandrofoglia07@gmail.com>
2025-01-17 23:03:42 +0100

    Add image input to main page

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,7 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Navbar from '@/components/Navbar';
 
 const MainPage: React.FC = () => {
+    const [image, setImage] = useState<string | null>(null);
+
+    const onImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        if (event.target.files && event.target.files[0]) {
+            setImage(URL.createObjectURL(event.target.files[0]));
+        }
+    };
+
     return (
         <div>
             <header className='h-20'>
@@ -9,6 +17,8 @@ const MainPage: React.FC = () => {
             </header>
             <main>
                 <div className='mx-auto my-24 max-w-[30rem] rounded-md p-6 shadow-sm sm:p-12 md:max-w-[40rem] md:p-24 lg:max-w-[60rem]'>MainPage</div>
+                <input type='file' onChange={onImageChange} />
+                {image && <img src={image} alt='preview' />}
             </main>
         </div>
     );
