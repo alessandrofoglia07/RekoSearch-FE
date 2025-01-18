d0227f313202fd0faeb2f00b81a68d1751cb1a2e
alessandrofoglia07 <alessandrofoglia07@gmail.com>
2025-01-18 22:39:07 +0100

    Add handleSubmit image function in MainPage

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,13 +1,27 @@
 import React, { useState } from 'react';
 import Navbar from '@/components/Navbar';
+import useAuth from '@/hooks/useAuth';
+import axios from '@/api/authAxios';
 
 const MainPage: React.FC = () => {
+    const { getSession } = useAuth();
     const [image, setImage] = useState<string | null>(null);
 
     const onImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         if (event.target.files && event.target.files[0]) {
             setImage(URL.createObjectURL(event.target.files[0]));
         }
+    };
+
+    const handleSubmit = async () => {
+        const session = await getSession();
+        if (!session) return console.log('User is not authenticated');
+        const formData = new FormData();
+        formData.append('image', image!);
+        const res = await axios.post('/image', formData, {
+            headers: { 'Content-Type': 'multipart/form-data' }
+        });
+        console.log(res.data);
     };
 
     return (
@@ -19,6 +33,10 @@ const MainPage: React.FC = () => {
                 <div className='mx-auto my-24 max-w-[30rem] rounded-md p-6 shadow-sm sm:p-12 md:max-w-[40rem] md:p-24 lg:max-w-[60rem]'>MainPage</div>
                 <input type='file' onChange={onImageChange} />
                 {image && <img src={image} alt='preview' />}
+                <button onClick={() => setImage(null)}>Remove</button>
+                <button disabled={image == null} onClick={handleSubmit}>
+                    Submit
+                </button>
             </main>
         </div>
     );
