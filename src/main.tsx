722399039d5b8f6a09ff48fd5477177d1ba863d9
alessandrofoglia07 <alessandrofoglia07@gmail.com>
2025-02-02 23:56:14 +0100

    Add aws-amplify dep and Amplify auth configuration

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,27 @@ import ReactDOM from 'react-dom/client';
 import App from '@/components/App';
 import '@/style.css';
 import { BrowserRouter } from 'react-router-dom';
-import { Account } from './context/AccountContext';
+import { Amplify } from 'aws-amplify';
+
+Amplify.configure({
+    Auth: {
+        Cognito: {
+            userPoolId: import.meta.env.VITE_AWS_COGNITO_USER_POOL_ID,
+            userPoolClientId: import.meta.env.VITE_AWS_COGNITO_USER_POOL_CLIENT_ID,
+            signUpVerificationMethod: 'code',
+            loginWith: {
+                email: true,
+                username: true,
+                phone: false
+            }
+        }
+    }
+});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
         <BrowserRouter>
-            <Account>
-                <App />
-            </Account>
+            <App />
         </BrowserRouter>
     </React.StrictMode>
 );
