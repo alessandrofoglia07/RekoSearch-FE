f70de342f34d51d5f8c8fe3b7585f83358ab87f0
alessandrofoglia07 <alessandrofoglia07@gmail.com>
2025-03-07 22:55:28 +0100

    Add basic skeleton for ImagePage and UserPage

--- src/pages/ImagePage.tsx
+++ src/pages/ImagePage.tsx
@@ -1,13 +1,24 @@
 import axios from '@/api/axios';
-import React, { useEffect } from 'react';
+import { CompleteImageResponse } from '@/types';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 const ImagePage: React.FC = () => {
     const { id } = useParams();
 
+    const [loading, setLoading] = useState(false);
+    const [image, setImage] = useState<CompleteImageResponse | null>(null);
+
     const getImageData = async (id: string) => {
-        const { data } = await axios.get(`/image/${id}`);
-        return data;
+        try {
+            setLoading(true);
+            const res = await axios.get(`/image/${id}`);
+            setImage(res.data);
+        } catch (err) {
+            console.log(err);
+        } finally {
+            setLoading(false);
+        }
     };
 
     useEffect(() => {
@@ -16,7 +27,30 @@ const ImagePage: React.FC = () => {
         getImageData(id);
     }, [id]);
 
-    return <div>ImagePage</div>;
+    // TODO: Implement the UI for the ImagePage
+    return (
+        <div>
+            {loading ? (
+                <div>Loading...</div>
+            ) : !image ? (
+                <div>Error</div>
+            ) : (
+                <div>
+                    <img src={image.fileUrl} alt={image.imageTitle} />
+                    <div>{image.imageTitle}</div>
+                    <div>{image.imageDescription}</div>
+                    <div>{image.category}</div>
+                    <div>{image.labels.join(', ')}</div>
+                    <div>
+                        {image.views} views | {image.likes} likes
+                    </div>
+                    <div>
+                        <a href={`/user/${image.authorUsername}`}>{image.authorUsername}</a>
+                    </div>
+                </div>
+            )}
+        </div>
+    );
 };
 
 export default ImagePage;
